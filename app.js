f2fb92ce41060bff2b645704dd1a3604fc654e8b
Julien Simonet <j.mons54@gmail.com>
2018-03-25 01:28:12 +0100

    minify css

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const minify = require('html-minifier').minify;
 const fs = require('fs');
 const path = require('path');
+const htmlMinify = require('html-minifier').minify;
+const cssoMinify = require('csso').minify;
 
-const html = minify(fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8'));
+const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
+fs.writeFile(path.join(__dirname, 'index.html'), htmlMinify(html));
 
-fs.writeFile(path.join(__dirname, 'index.html'), html, function (err) {
-  if (err) throw err;
-  console.log('Saved!');
-});
+
+const cssPath = path.join(__dirname, 'assets/css');
+const css = readAllFiles(cssPath, [
+    'material-kit.css',
+    'index.css'
+]);
+
+fs.writeFile(path.join(cssPath, 'app.min.css'), cssoMinify(css).css);
+
+function readAllFiles(folder, files, data = '') {
+
+    files.forEach(function (value) {
+        data += fs.readFileSync(path.join(folder, value), 'utf-8');
+    });
+
+    return data;
+}
