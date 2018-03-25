def5046a201c2a9a22994e769326a1bb4b54170e
Julien Simonet <j.mons54@gmail.com>
2018-03-25 03:34:29 +0200

    minify js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const htmlMinify = require('html-minifier').minify;
 const cssoMinify = require('csso').minify;
+const jsMinify = require('uglify-js').minify;
+
 
 const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
 fs.writeFile(path.join(__dirname, 'index.html'), htmlMinify(html));
-
 
 const cssPath = path.join(__dirname, 'assets/css');
 const css = readAllFiles(cssPath, [
@@ -14,6 +15,18 @@ const css = readAllFiles(cssPath, [
 ]);
 
 fs.writeFile(path.join(cssPath, 'app.min.css'), cssoMinify(css).css);
+
+const jsPath = path.join(__dirname, 'assets/js');
+const js = readAllFiles(jsPath, [
+    'jquery.min.js',
+    'popper.min.js',
+    'bootstrap-material-design.js',
+    'nouislider.min.js',
+    'material-kit.js',
+    'index.js'
+]);
+
+fs.writeFile(path.join(jsPath, 'app.min.js'), jsMinify(js).code);
 
 function readAllFiles(folder, files, data = '') {
 
