4014857c7eb63195217bdc9a60d2a2259f70b507
Julien <julien.simonet@missnumerique.com>
2018-03-30 18:11:36 +0200

    optimize images

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
-const fs = require('fs');
-const path = require('path');
+const { lstatSync, readdirSync, writeFile, readFileSync } = require('fs');
+const { join } = require('path');
 const htmlMinify = require('html-minifier').minify;
 const cssoMinify = require('csso').minify;
 const jsMinify = require('uglify-js').minify;
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
 
-const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf-8');
-fs.writeFile(path.join(__dirname, 'index.html'), htmlMinify(html));
 
-const css = readAllFiles(path.join(__dirname, 'assets/css'), [
+const readAllFiles = (folder, files, data = '') => { 
+    files.forEach(value => {
+        data += readFileSync(join(folder, value), 'utf-8'); 
+    });
+    return data;
+};
+
+const getDirectories = source => 
+    readdirSync(source).map(name => 
+        join(source, name)).filter(source => lstatSync(source).isDirectory());
+
+const optimizeImages = source => {
+
+    if (!source) {
+        return;
+    }
+
+    getDirectories(source).forEach(folder => {
+        imagemin([folder + '/*.{jpg,png}'], folder, {
+            plugins: [
+                imageminJpegtran(),
+                imageminPngquant({quality: '65-80'})
+            ]
+        }).then(files => {
+            console.log('Optimize images', folder);
+        });
+
+        optimizeImages(folder);
+    });
+};
+
+optimizeImages(join(__dirname, 'assets/img/products'));
+
+const html = readFileSync(join(__dirname, 'index.html'), 'utf-8');
+writeFile(join(__dirname, 'index.html'), htmlMinify(html), function () {
+    console.log('HTML is minify');
+});
+
+const css = readAllFiles(join(__dirname, 'assets/css'), [
     'material-kit.css',
     'index.css'
 ]);
 
-fs.writeFile(path.join(__dirname, 'assets/app.min.css'), cssoMinify(css).css);
+writeFile(join(__dirname, 'assets/app.min.css'), cssoMinify(css).css, function () {
+    console.log('CSS is minify');
+});
 
-const js = readAllFiles(path.join(__dirname, 'assets/js'), [
+const js = readAllFiles(join(__dirname, 'assets/js'), [
     'jquery.min.js',
     'popper.min.js',
     'bootstrap-material-design.js',
@@ -23,13 +64,7 @@ const js = readAllFiles(path.join(__dirname, 'assets/js'), [
     'index.js'
 ]);
 
-fs.writeFile(path.join(__dirname, 'assets/app.min.js'), jsMinify(js).code);
+writeFile(join(__dirname, 'assets/app.min.js'), jsMinify(js).code, function () {
+    console.log('JS is minify');
+});
 
-function readAllFiles(folder, files, data = '') {
-
-    files.forEach(function (value) {
-        data += fs.readFileSync(path.join(folder, value), 'utf-8');
-    });
-
-    return data;
-}
